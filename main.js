3a064f6dba019ee1ab736d01e095f5b279d94c84
HirokoS <hirokoyzs@hotmail.com>
2020-07-07 15:50:14 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let next = document.getElementById("next");
+let lemonade = document.getElementById("lemonade");
+let recipe = document.getElementById("recipe");
+
+next.onclick = function() {
+    recipe.style.display = 'block';
+}
+
